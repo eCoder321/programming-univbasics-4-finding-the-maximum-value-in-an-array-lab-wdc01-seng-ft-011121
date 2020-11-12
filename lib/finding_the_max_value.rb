75a396f9954b8372ba674b49f4e789a8694ffa06
Fem Adisa <femiwonderful@gmail.com>
2020-11-12 10:31:44 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,10 @@
 def find_max_value(array)
-  sorted = array.sort 
-  return sorted[-1]
+# sorted = array.sort 
+# return sorted[-1]
+
+  reference = 0 
+  for i in array
+    reference = i if i >= reference
+  end
+  reference
 end
